c21bcf0541cd896a4d61553f3df903015e1a37c0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-09-07 07:44:42 +0900

    Rack 3 released.

--- gems/falcon-v0-rack-v3.rb
+++ gems/falcon-v0-rack-v3.rb
@@ -6,7 +6,7 @@
 eval_gemfile '../gems.rb'
 
 gem "falcon", "~> 0.0"
-gem "rack", "~> 3.0.0.rc1"
+gem "rack", "~> 3.0.0"
 
 # export RACK_CONFORM_SERVER="falcon serve"
 # export RACK_CONFORM_ENDPOINT="https://localhost:9292"
