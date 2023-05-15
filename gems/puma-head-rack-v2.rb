10d456228ea51ae03cb52bd0a99ebd8a2fe88d5b
MSP-Greg <Greg.mpls@gmail.com>
2023-05-15 13:17:10 -0500

    [CI] Add Puma head and Rack 2 job

---
+++ gems/puma-head-rack-v2.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2022, by Samuel Williams.
+
+eval_gemfile '../gems.rb'
+
+gem "puma", git: "https://github.com/puma/puma.git"
+gem "rack", "~> 2.0"
+
+# export RACK_CONFORM_SERVER="puma"
+# export RACK_CONFORM_ENDPOINT="http://localhost:9292"
