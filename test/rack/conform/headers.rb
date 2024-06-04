7f24a3962967f94f27cb126c529fa900149d62fd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-04 11:59:07 +0900

    Add conformance test for request headers.

--- test/rack/conform/headers.rb
+++ test/rack/conform/headers.rb
@@ -6,17 +6,32 @@
 require 'client_context'
 include ClientContext
 
-def body(headers)
-	Protocol::HTTP::Body::Buffered.wrap(JSON.generate(headers))
+with 'response headers' do
+	def body(headers)
+		Protocol::HTTP::Body::Buffered.wrap(JSON.generate(headers))
+	end
+
+	it 'can echo back headers' do
+		response = client.get("/headers/response", {}, body({'content-type' => 'text/plain'}))
+		
+		expect(response.status).to be == 200
+		expect(response.headers).to have_keys(
+			'content-type' => be == 'text/plain'
+		)
+	ensure
+		response&.finish
+	end
 end
 
-it 'can echo back headers' do
-	response = client.get("/headers", {}, body({'content-type' => 'text/plain'}))
-	
-	expect(response.status).to be == 200
-	expect(response.headers).to have_keys(
-		'content-type' => be == 'text/plain'
-	)
-ensure
-	response&.finish
+with 'request headers' do
+	it 'can echo back headers with multiple set-cookie values' do
+		response = client.get("/headers/request", [['set-cookie', 'a=1'], ['set-cookie', 'b=2']])
+		
+		expect(response.status).to be == 200
+		headers = JSON.parse(response.read)
+		
+		expect(headers).to have_keys(
+			'HTTP_SET_COOKIE' => be == "a=1;b=2"
+		)
+	end
 end
