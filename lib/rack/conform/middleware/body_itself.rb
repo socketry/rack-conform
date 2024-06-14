daa4050ed3436284a399316ba53e6ffc3d89bc6e
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-06-15 01:59:49 +0200

    Add test for a middleware that returns itself as the body (#25)

---
+++ lib/rack/conform/middleware/body_itself.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Earlopain.
+
+module Rack
+  module Conform
+    module Middleware
+      class BodyItself
+        def initialize(app)
+          @app = app
+        end
+      
+        def each
+          yield "Hello World"
+        end
+      
+        def call(env)
+          [200, {}, self]
+        end
+      end
+    end
+  end
+end
---
+++ test/rack/conform/middleware.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Earlopain.
+
+require 'client_context'
+include ClientContext
+
+require 'rack'
+
+it 'can handle a middleware that returns itself as the body' do
+  response = client.get("/middleware/body/itself")
+  
+  expect(response.status).to be == 200
+  expect(response.read).to be == "Hello World"
+ensure
+  response&.finish
+end
