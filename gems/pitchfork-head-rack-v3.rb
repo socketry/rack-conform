2c0412a548e32ef8a68074bfa77d2499064c4642
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-15 00:13:22 +0900

    Add pitchfork on Rack 3.

---
+++ gems/pitchfork-head-rack-v3.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
+eval_gemfile '../gems.rb'
+
+gem "pitchfork", git: "https://github.com/Shopify/pitchfork.git"
+gem "rack", "~> 3.0"
+
+# export RACK_CONFORM_SERVER="pitchfork -E none"
+# export RACK_CONFORM_ENDPOINT="http://localhost:8080"
